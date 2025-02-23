e9d6fabd7116faa56cfa2e2652d135a6657eb6e8
akash-helium <akash@gethelium.co>
2025-02-23 13:40:48 +0530

    cases section revmoed '

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       <Hero />
       <BrandMaqrquee />
       <PracticeAreasSection />
-      <CasesSection />
       <CTASection />
       <AttorneysSection />
       <Why />
